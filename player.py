70ed367d7e59ff1a0ed04bf553e10cffe9421da0
hmokcode <mart1n.krocil@seznam.cz>
2025-01-15 23:25:18 +0100

    drew triangle into the middle

--- player.py
+++ player.py
@@ -1,9 +1,10 @@
+import pygame
 from circleshape import CircleShape
 from constants import PLAYER_RADIUS
 
 class Player(CircleShape):
     def __init__(self, x, y):
-        super().__init__(PLAYER_RADIUS)
+        super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
     
     def triangle(self):
@@ -13,3 +14,6 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
